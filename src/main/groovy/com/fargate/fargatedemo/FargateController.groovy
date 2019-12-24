36108f0d48c9d0f18a7b7e287453d27beb77cff7
Roger Diller <roger@flexrentalsolutions.com>
2019-12-23 20:44:31 -0500

    getting a joke to generate n stuff

--- src/main/groovy/com/fargate/fargatedemo/FargateController.groovy
+++ src/main/groovy/com/fargate/fargatedemo/FargateController.groovy
@@ -3,14 +3,36 @@ package com.fargate.fargatedemo
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+
+import java.lang.management.ManagementFactory
 
 @RestController
 @RequestMapping("/")
 class FargateController {
 
-    @GetMapping("")
-    String fargate(){
+    int jokeRequestCount = 0
 
-        return 'YO!!'
+    @GetMapping("")
+    String fargateDemo(){
+        return "Hey there internet person... you've stumbled upon Roger's Fargate Demo! Hit /chuck for a random joke. :)"
+    }
+
+    @GetMapping("/chuck")
+    Map chuckNorrisJoke(){
+
+        jokeRequestCount++
+
+        def jokeJson = new RestTemplate().getForObject('http://api.icndb.com/jokes/random', Object)
+
+        Map mapResult = [:]
+
+        //noinspection GrUnresolvedAccess
+        mapResult.joke = jokeJson?.value?.joke as String
+        mapResult.requestNumber = jokeRequestCount
+        mapResult.jvm = ManagementFactory.getRuntimeMXBean().getName()
+        mapResult.javaVersion = System.getProperty("java.version")
+
+        return mapResult
     }
 }
