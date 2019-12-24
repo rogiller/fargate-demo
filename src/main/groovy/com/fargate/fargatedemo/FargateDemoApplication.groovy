7f3b5c4dd1e0856c24bccee77288fa4a2105e270
Roger Diller <roger@flexrentalsolutions.com>
2019-12-23 20:20:27 -0500

    right after start.spring.io download

---
+++ src/main/groovy/com/fargate/fargatedemo/FargateDemoApplication.groovy
@@ -0,0 +1,13 @@
+package com.fargate.fargatedemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FargateDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FargateDemoApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/fargate/fargatedemo/FargateDemoApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.fargate.fargatedemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FargateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
