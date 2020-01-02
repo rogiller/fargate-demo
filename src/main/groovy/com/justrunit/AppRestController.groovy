7807df5b07130bcf9591118a4ee475fc5d011266
Roger Diller <roger@flexrentalsolutions.com>
2020-01-02 15:20:40 -0500

    imports

--- src/main/groovy/com/justrunit/AppRestController.groovy
+++ src/main/groovy/com/justrunit/AppRestController.groovy
@@ -1,6 +1,5 @@
 package com.justrunit
 
-import groovy.transform.Memoized
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
